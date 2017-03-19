ca4a3e923022a2a66350d8ca49ab74f5bee8c5b2
Rubi Cabral <rubisc@gmail.com>
2017-03-19 16:55:43 -0700

    added update function to users controller

--- controllers/users.js
+++ controllers/users.js
@@ -7,16 +7,40 @@ function index(req, res) {
   })
 }
 
-
 function create(req, res){
   var newUser = new User(req.body)
-  newUser.save(function(err, saveUser){
+  newUser.save(function(err, saveUser) {
     if (err) throw err
     res.json(saveUser)
   })
 }
 
+// function update(req, res) {
+//   User.findOneAndUpdate({id: req.params.id}, {username: req.body.username, expLevel: req.body.expLevel}, function(err, user) {
+//     if (err) throw err
+//     res.json(user)
+//   })
+// }
+
+function update(req, res) {
+//update function
+var id = req.params._id
+User.findById(id, function(err, user) {
+  if (err) throw err
+  // change user username and expLevel
+  user.username = req.body.username
+  user.expLevel = req.body.expLevel
+  //save the user
+  User.save(function(err) {
+    if (err) throw err
+    res.json('User successfully saved!')
+   })
+ })
+}
+
+
 module.exports = {
   index: index,
-  create: create
+  create: create,
+  update: update
 }
