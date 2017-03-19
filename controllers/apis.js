68303671fa393acf5d69854e4fe0928249ffc00f
Rubi Cabral <rubisc@gmail.com>
2017-03-19 13:34:00 -0700

    api controller with index and show

--- controllers/apis.js
+++ controllers/apis.js
@@ -1,1 +1,15 @@
 var Api = require('../models/api')
+
+function index(req, res) {
+  Api.find({}, function(err, apis) {
+    if (err) throw err
+    res.json(apis)
+  })
+}
+
+function show(req, res) {
+  Api.find({}, function(err, user) {
+    if (err) throw
+    res.json(api)
+  })
+}
