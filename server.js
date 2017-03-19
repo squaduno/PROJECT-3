4c2c9c4036174a4fd3e882604a1896614ccfb897
alex espinoza <alexjespinozac@gmail.com>
2017-03-19 13:27:28 -0700

    maintenance commit

--- server.js
+++ server.js
@@ -1,2 +1,51 @@
 var express = require('express'),
-    
+    // path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    port = process.env.PORT || 3000,
+    app = express()
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'))
+// app.set('view engine', 'ejs')
+
+// middleware
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+// app.use(logger('dev'))
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(cookieParser())
+// app.use(express.static(path.join(__dirname, 'public')))
+
+root route
+var main = require()
+app.get('/', function(req, res){
+  res.send('Hello World')
+})
+
+
+
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found')
+//   err.status = 404
+//   next(err)
+// })
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message
+//   res.locals.error = req.app.get('env') === 'development' ? err : {}
+//
+//   // render the error page
+//   res.status(err.status || 500)
+//   res.render('error')
+// })
+
+app.listen(port, function(req, res){
+  console.log('The server is running on port', port)
+})
