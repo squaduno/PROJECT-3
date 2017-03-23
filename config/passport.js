bab48a62afd66822538497457e6d58fc287db4d4
alex espinoza <alexjespinozac@gmail.com>
2017-03-23 10:00:47 -0700

    gets user name or email or welcome message from GitHub Oauth

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
-var LocalStrategy     = require('passport-local').Strategy,
-    User              = require('../models/user'),
-    GitHubStrategy    = require('passport-github').Strategy;
+var LocalStrategy = require('passport-local').Strategy,
+  User = require('../models/user'),
+  GitHubStrategy = require('passport-github').Strategy;
 
 
 module.exports = function(passport) {
@@ -11,7 +11,7 @@ module.exports = function(passport) {
 
   passport.deserializeUser(function(id, callback) {
     User.findById(id, function(err, user) {
-        callback(err, user);
+      callback(err, user);
     });
   });
 
@@ -23,19 +23,21 @@ module.exports = function(passport) {
     process.nextTick(function() {
 
       // Find a user with this e-mail
-      User.findOne({ 'local.email' :  email }, function(err, user) {
+      User.findOne({
+        'local.email': email
+      }, function(err, user) {
         if (err) return callback(err);
 
         // If there already is a user with this email
         if (user) {
           return callback(null, false, req.flash('signupMessage', 'This email is already used.'));
         } else {
-        // There is no email registered with this email
+          // There is no email registered with this email
 
           // Create a new user
-          var newUser            = new User();
-          newUser.local.name     = req.body.name;
-          newUser.local.email    = email;
+          var newUser = new User();
+          newUser.local.name = req.body.name;
+          newUser.local.email = email;
           newUser.local.password = newUser.encrypt(password);
 
           newUser.save(function(err) {
@@ -53,66 +55,70 @@ module.exports = function(passport) {
     passReqToCallback: true
   }, function(req, email, password, callback) {
 
-     // Search for a user with this email
-     User.findOne({ 'local.email' :  email }, function(err, user) {
-       if (err) return callback(err);
-
-        // If no user is found
-       if (!user) return callback(null, false, req.flash('loginMessage', 'No user found.'));
-
-       // Wrong password
-       if (!user.validPassword(password))
-       return callback(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
-
-       return callback(null, user);
-     });
-   }));
-
-//////////////////////////GITHUB//////////////////////////////////
-passport.use(new GitHubStrategy({
-    // authorizationURL: 'https://github.com/login/oauth/authorize',
-    // tokenURL: 'https://github.com/login/oauth/token',
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: process.env.GITHUB_CALL_BACK_URL,
-
-  },
-
-  // CREATES A NEW USER WITH INFO FROM GITHUB
-  function(accessToken, refreshToken, profile, callback) {
+    // Search for a user with this email
     User.findOne({
-      github: {id: profile.id
-    }}, function(err, user) {
-      if (err)
-        return callback(err);
-      if (user) {
-        return callback(null, user);
-      } else {
-        var newUser = new User();
-        // console.log('GitHub profile:' + JSON.stringify(profile._json))
-        newUser.github = {
-          id: profile.id,
-          token: accessToken,
-          name: profile._json.name,
-          // email: profile._json.email
-        }
-        if (profile._json.email !== null){
-newUser.local.email = profile._json.email
-} else if (profile._json.email == null){
-newUser.local.email = 'Guest visiting us from GitHub'
-}
+      'local.email': email
+    }, function(err, user) {
+      if (err) return callback(err);
 
-newUser.local.name = profile._json.name;
-console.log(newUser)
+      // If no user is found
+      if (!user) return callback(null, false, req.flash('loginMessage', 'No user found.'));
 
-        newUser.save(function(err) {
-          if (err)
-            throw err;
-          return callback(null, newUser);
-        })
-      }
+      // Wrong password
+      if (!user.validPassword(password))
+        return callback(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+
+      return callback(null, user);
     });
-  }
-));
+  }));
+
+  //////////////////////////GITHUB//////////////////////////////////
+  passport.use(new GitHubStrategy({
+      // authorizationURL: 'https://github.com/login/oauth/authorize',
+      // tokenURL: 'https://github.com/login/oauth/token',
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.GITHUB_CALL_BACK_URL,
+
+    },
+
+    // CREATES A NEW USER WITH INFO FROM GITHUB
+    function(accessToken, refreshToken, profile, callback) {
+      User.findOne({
+        github: {
+          id: profile.id
+        }
+      }, function(err, user) {
+        if (err)
+          return callback(err);
+        if (user) {
+          return callback(null, user);
+        } else {
+          var newUser = new User();
+          // console.log('GitHub profile:' + JSON.stringify(profile._json))
+          newUser.github = {
+            id: profile.id,
+            token: accessToken,
+            name: profile._json.name,
+            email: profile._json.email
+          }
+          // GETS USERS EMAIL, NAME OR CUSTOM WELCOME MESSAGE FOR GITHUB USERS
+          if (profile._json.email !== null) {
+            newUser.local.email = profile._json.email
+          } else if (profile._json.email == null && profile._json.name !== null) {
+            newUser.local.email = profile._json.name
+          } else {
+            newUser.local.name = "Guest visiting us from GitHub";
+          }
+          console.log(newUser)
+          newUser.save(function(err) {
+            if (err)
+              throw err;
+            return callback(null, newUser);
+          })
+        }
+      });
+    }
+  ));
 
 }
